c4bd677225e1eae20e2e0ecf7f1d1a2619e16a3c
MohammadAl-Fartousy <Mohammed_al-fartousy@hotmail.com>
2022-03-14 14:40:21 +0000

    new files

---
+++ src/main/java/com/qa/lions/SpringLionsApplication.java
@@ -0,0 +1,13 @@
+package com.qa.lions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/lions/SpringLionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.lions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
