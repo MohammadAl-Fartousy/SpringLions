2cbd544825c4a06a7160c6b0e604c478cc15c1db
MohammadAl-Fartousy <Mohammed_al-fartousy@hotmail.com>
2022-03-14 15:39:12 +0000

    new files

--- src/main/java/com/qa/lions/service/ServiceMethods.java
+++ src/main/java/com/qa/lions/service/ServiceMethods.java
@@ -1,5 +1,22 @@
 package com.qa.lions.service;
+import java.util.List;
 
-public class ServiceMethods {
+public interface ServiceMethods<L> {
 
+	
+	L create(L lion);
+	
+	
+	List<L> readAll();
+	
+
+	L readById(long id);
+	
+	
+	L update(long id, L duck);
+	
+	
+	boolean delete(long id);
+	
+	
 }
